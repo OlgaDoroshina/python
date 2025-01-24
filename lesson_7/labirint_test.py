8bbf99854c2848caa5fe7dee427a123ca954196f
Ольга Дорошина <you@example.com>
2025-01-24 09:07:59 +0300

    8

--- lesson_7/labirint_test.py
+++ lesson_7/labirint_test.py
@@ -1,6 +1,7 @@
 
+from pytest
 from time import sleep
-from selenium import webdriver
+from selenium import
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
