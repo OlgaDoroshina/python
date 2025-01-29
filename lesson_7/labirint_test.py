f7a70b78f4a37f9fa7df43ff37f233a2a81b1488
Ольга Дорошина <you@example.com>
2025-01-29 14:06:27 +0300

    new2

--- lesson_7/labirint_test.py
+++ lesson_7/labirint_test.py
@@ -1,27 +1,44 @@
-
-from pytest
 from time import sleep
-from selenium import
-from selenium.webdriver.chrome.service import Service
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.color import Color
+from selenium.webdriver.common.keys import Keys
 
 
 cookie = {"name": "cookie_policy", "value": "1"}
 
 
 def test_card_counter():
-    browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-  
-    
+    driver = webdriver.Chrome(
+        service=ChromeService(ChromeDriverManager().install()))
+
     # Перейти на сайт «Лабиринта»
-    browser.get("https://www.labirint.ru/")
-    browser.implicitly_wait(4)
-    browser.maximize_window()
-    browser.add_cookie(cookie)
-    sleep(5)
-    # Найти все книги по слову Python
+    driver.get("https://www.labirint.ru/")
+    driver.implicitly_wait(4)
+    driver.maximize_window()
+    driver.add_cookie(cookie)
+    # Найти все книги по слову python
+    driver.find_element(By.CSS_SELECTOR, "#search-field").send_keys('python')
+    driver.find_element(By.CSS_SELECTOR, "button[type=submit]").click()
     # Переключиться на таблицу
-    # Добавить все книги в корзину и посчитать
+    # Добавить все книги на первой странице в корзину и посчитать
+    buy_buttons = driver.find_elements(
+        By.CSS_SELECTOR, "a._btn.btn-tocart.buy-link")
+
+    counter = 0
+    for btn in buy_buttons:
+        btn.click()
+        counter += 1
     # Перейти в корзину
-    # Проверить счетчик товаров
-    browser.quit()
+    driver.get("https://www.labirint.ru/cart/")
+
+    # Проверить счетчик товаров. Должен быть равен числу нажатий
+    # Получить текущее значение
+    txt = driver.find_element(
+        By.CSS_SELECTOR, "a[data-event-label='myCart']").find_element(By.CSS_SELECTOR, 'b').text
+
+    # Сравнить c counter
+    assert counter == int(txt)
+    driver.quit()
