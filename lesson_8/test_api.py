96af770a5870d8960d4579d1f8281ec281980d1f
Ольга Дорошина <you@example.com>
2025-02-10 18:18:30 +0300

    uuu

--- lesson_8/test_api.py
+++ lesson_8/test_api.py
@@ -1,62 +1,114 @@
 import requests
-from file import SkyToDo
-from faker import Faker
 
-fake = Faker()
+base_url = "https://yougile.com"
 
-url = "https://todo-app-sky.herokuapp.com"
+## Авторизация
+my_login = 'olga27.skypro@gmail.com'
+my_password = 'Olgasky'
 
-api = SkyToDo(url)
+# Получить список компаний
+def test_auth():
+    creds = {
+        'login': my_login,
+        'password': my_password
+    }
+    resp = requests.post(base_url + '/api-v2/auth/companies', json=creds)
+    response_body = resp.json()["content"][0]
+    first_company = response_body["id"]
+    assert resp.status_code == 200
+    assert len(response_body) > 0
+    return first_company
 
-# - Создание.
-def test_create_task():
-    response, new_t = api.create_task()
-    assert response.status_code == 201
-    assert response.json()["title"] == new_t["title"]
-    
-# - Переименование.
-def test_rename_task():
-    resp1, new_t = api.create_task()
-    task_url = resp1.json()["url"]
-    resp2 = api.chang_title(task_url)
-    assert resp2.status_code == 200
-    assert resp2.json()["title"] != new_t
-    
-# - Удаление.
-def test_delete_task():
-    resp1 = api.create_task()[0]
-    task_url = resp1.json()["url"]
-    resp2 = api.delete_task(task_url)
-    assert resp2.status_code == 204
+# Создать ключ
+def test_key():
+     creds = {
+        'login': my_login,
+        'password': my_password,
+        'companyId': test_auth()
+     }
+     resp = requests.post(base_url + '/api-v2/auth/keys', json=creds)
+     response_body = resp.json()["key"]
+     assert resp.status_code == 201
+     return response_body
 
-# - Получение списка.
-def test_get_list():
-    response = api.get_list()
-    to_do_list = response.json
-    print(to_do_list)
-    assert response.status_code == 200
-    # assert to_do_list 
-    
-# - Получение конкретной задачи из списка.
-def test_get_one_task(number = 0):
-    task_url = api.get_task(number)["url"]
-    task_id = api.get_task(number)["id"]
-    assert task_url == f'/{task_id}'
+# получить токен 
+def test_token():
+    my_token = {
+        'Authorization': ("Bearer " + test_key())
+    }
+    return my_token
 
-def task_is_to_do(number = -1, donable=None):
-    task_url = api.get_task(number)["url"]
-    response = api.task_done(task_url, donable)
-    assert response.status_code == 200
-    assert response.json()["completed"] == donable
+# Получить ИД сотрудника
+def test_ID_worker():
+     resp = requests.get(base_url+'/api-v2/users', headers=test_token())
+     response_body = resp.json()["content"][0]
+     worker = response_body["id"]
+     assert resp.status_code == 200
+     return worker
 
-# - Отметка задачи «Выполнена».
-def test_task_get_done():
-    task_is_to_do(0,True)
+## Позитивные проверки
+# Получить список проектов (надо исп полученный ключ в запросе)
+def test_get_project():
+    resp = requests.get(base_url+'/api-v2/projects', headers=test_token())
+    assert resp.status_code == 200
 
-# - Снятие отметки «Выполнена».
-def test_task_to_do():
-    task_is_to_do(-1, False)
+# создать проект (надо получить ид сотрудника) 
+def test_new_project():
+    project = {
+        "title": "Новый проект",
+        "users": {
+             test_ID_worker(): "admin"
+        }
+    }
+    resp = requests.post(base_url+'/api-v2/projects', json=project, headers=test_token())
+    response_body = resp.json()["id"]
+    assert resp.status_code == 201
+    return response_body
 
-def test_terminate_list():
-    api.terminate_list()
-    assert api.get_list().json() == []
+# Получить по ID проект
+def test_id_project():
+    id_project = test_new_project()
+    resp = requests.get(base_url+'/api-v2/projects/'+id_project, headers=test_token())
+    response_body = resp.json()["id"]
+    assert id_project == response_body
+    assert resp.status_code == 200
+
+# изменить проект 
+def test_change_project():
+    id_project = test_new_project()
+    creds = {
+        "title": "Очень новый проект"
+        }
+    resp = requests.put(base_url+'/api-v2/projects/'+id_project, json=creds, headers=test_token())
+    assert resp.status_code == 200
+
+
+## Негативные проверки
+# Получить список проектов без авторизации
+def test_noget_project():
+    resp = requests.get(base_url+'/api-v2/projects')
+    assert resp.status_code == 401
+
+# создать проект без ИД сотрудника
+def test_nonew_project():
+    project = {
+        "title": "Новый проект",
+        "users": {
+             test_ID_worker(): ""
+        }
+    }
+    resp = requests.post(base_url+'/api-v2/projects', json=project, headers=test_token())
+    assert resp.status_code == 400
+
+# Получить проект по несуществующему ID 
+def test_noid_project():
+    resp = requests.get(base_url+'/api-v2/projects/'+"4567899", headers=test_token())
+    assert resp.status_code == 404
+
+# изменить не существующий проект 
+def test_nochange_project():
+    creds = {
+        "title": "Очень новый проект"
+        }
+    resp = requests.put(base_url+'/api-v2/projects/'+"4567899", json=creds, headers=test_token())
+    assert resp.status_code == 404
