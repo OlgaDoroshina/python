814264805655b06a21fb4ae78a95c2fb0ada176e
OlgaDoroshina <Monik5@yandex.ru>
2025-03-24 21:16:07 +0300

    Delete Diplom QA/setting.py

--- Diplom QA/setting.py
+++
@@ -1,7 +0,0 @@
-ui_url = "https://chitai-gorod.ru"
-api_url = "https://web-gate.chitai-gorod.ru/"
-access_token = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL3VzZXItcmlnaHQiLCJzdWIiOjIxMzc0MDQ5LCJpYXQiOjE3MzY2ODEwODcsImV4cCI6MTczNjY4NDY4NywidHlwZSI6MjB9.Wq3nhdx4wAEsRNZgUFQH9UXvXtX0MQ6W-BgOz5IKgzo"
-my_headers = {
-    "Content-Type": "application/json",
-    "Authorization": access_token,
-}
